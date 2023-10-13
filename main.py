612111dc1205f98cdf454935369bd964518cc50a
Daniel Rosel <daniel@alves.world>
2023-10-13 14:41:58 +0200

    basic pygame implementation

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+"""
+ADS Final Project
+=================
+A black-jack implementation in Python.
+Using:
++ Pygame
+"""
+
+import pygame
+import sys
+
 
 def main():
-    pass
+    pygame.init()
+    pygame.display.set_caption('Black Jack')
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.update()
+        clock.tick(60)
+
 
 if __name__ == '__main__':
     main()
